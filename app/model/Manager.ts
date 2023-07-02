f9be572b2e06c288c20776f43802525d4ddc953b
Henrique Shen <hshen2908@gmail.com>
2023-07-02 01:51:07 -0300

    Removed unnecessary require

--- app/model/Manager.ts
+++ app/model/Manager.ts
@@ -1,25 +1,6 @@
 import {AppMetadata, ManagementClient, ObjectWithId, Permission, User, UserMetadata} from "auth0";
 
 require("dotenv").config();
-const axios = require("axios").default;
-
-// const getManagementAPIAccessTokenOptions = {
-//     method: 'POST',
-//     url: `${process.env.APPLICATION_ISSUER_BASE_URL}/oauth/token`,
-//     headers: {'content-type': 'application/x-www-form-urlencoded'},
-//     data: new URLSearchParams({
-//         grant_type: 'client_credentials',
-//         client_id: process.env.APPLICATION_CLIENT_ID,
-//         client_secret: process.env.CLIENT_SECRET,
-//         audience: `${process.env.APPLICATION_ISSUER_BASE_URL}/api/v2/`
-//     })
-// };
-//
-// axios.request(getManagementAPIAccessTokenOptions).then(function (response) {
-//     console.log(response.data);
-// }).catch(function (error) {
-//     console.error(error);
-// });
 
 const managementClient = new ManagementClient({
     domain: `${process.env.ACCOUNT_DOMAIN}.auth0.com`,
